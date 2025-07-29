4b4da61e73ec43d3a4aac48d3d6037d0f2e826ab
wn-marie <mw270761@gmail.com>
2025-07-29 20:31:50 +0300

    Added Intro.py

---
+++ Intro.py
@@ -0,0 +1,46 @@
+#PLP = "Academy" 
+
+# Student_Name= "John Doe"
+# Student_ID= "123456"
+# Student_Age= 20
+# Fees_clearance= True
+
+#output
+# print(Student_Name, Student_Age, Student_ID, Fees_clearance, )
+
+#Lists
+# languages = ["Python", "Java", "C++", "JavaScript"]
+# print(languages)
+
+# shopping_list = ["Milk", "Eggs", "Bread", "Butter"]
+# print(shopping_list)
+
+# # Tuples
+# products = ('XBox', 499.99, "Habibi", 23)
+# print(products)
+
+#Sets
+# Set_A = {1, 2, 3, 4, 5}
+# Set_B = {4, 5, 6, 7, 8}
+# print(Set_A)
+# print(Set_B)
+
+#Dictionaries
+# student_info = {
+#     "Name": "John Doe",
+#     "ID": "123456",
+#     "Age": 20,
+#     "Fees_clearance": True
+# }
+# print(student_info)
+
+# capital_city = {"Kenya": "Nairobi", "Nigeria": "Lagos", "Uganda": "Kampala", "South Africa": "Cape Town"}
+# print(capital_city)
+
+#Logical Operators
+a = 10 
+b = 20
+c = 30
+print(a < b and b < c)  # True  
+print(a < b or b > c)   # True 
+print(not (a < b))      # False
